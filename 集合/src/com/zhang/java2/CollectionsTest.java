6a2ef11caf821cfbdf878c2ebf8a6084d4363990
Dotown <1242262161@qq.com>
2021-01-07 01:59:27 +0800

    集合

---
+++ 集合/src/com/zhang/java2/CollectionsTest.java
@@ -0,0 +1,88 @@
+package com.zhang.java2;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Collections:操作Collection、Map的工具类
+ *
+ * 面试题：Collection 和 Collections的区别？
+ *
+ * author PC
+ * create 2021-01-07-1:43
+ */
+public class CollectionsTest {
+    /*
+reverse(List)：反转 List 中元素的顺序
+shuffle(List)：对 List 集合元素进行随机排序
+sort(List)：根据元素的自然顺序对指定 List 集合元素按升序排序
+sort(List，Comparator)：根据指定的 Comparator 产生的顺序对 List 集合元素进行排序
+swap(List，int， int)：将指定 list 集合中的 i 处元素和 j 处元素进行交换
+
+Object max(Collection)：根据元素的自然顺序，返回给定集合中的最大元素
+Object max(Collection，Comparator)：根据 Comparator 指定的顺序，返回给定集合中的最大元素
+Object min(Collection)
+Object min(Collection，Comparator)
+int frequency(Collection，Object)：返回指定集合中指定元素的出现次数
+void copy(List dest,List src)：将src中的内容复制到dest中
+boolean replaceAll(List list，Object oldVal，Object newVal)：使用新值替换 List 对象的所有旧值
+ */
+
+    @Test
+    public void test1(){
+        ArrayList arrayList = new ArrayList();
+        arrayList.add(123);
+        arrayList.add(456);
+        arrayList.add(789);
+        arrayList.add(987);
+        arrayList.add(-555);
+
+//        ArrayList arrayList1 = new ArrayList(); //src.size()>dest.size()则抛出IndexOutOfBoundsException异常。
+        List arrayList1 = Arrays.asList(new Object[arrayList.size()]);  //这样arrayList含有五个为null的元素
+        Collections.copy(arrayList1,arrayList);
+        System.out.println(arrayList1);
+    }
+
+    @Test
+    public void test2(){
+        /*
+        Collections 类中提供了多个 synchronizedXxx() 方法，
+        该方法可使将指定集合包装成线程同步的集合，从而可以解决
+        多线程并发访问集合时的线程安全问题
+         */
+        List list = new ArrayList();
+        list.add(123);
+        list.add(43);
+        list.add(765);
+        list.add(-97);
+        list.add(0);
+        List list1 = Collections.synchronizedList(list);//返回的list1即为线程安全的List
+    }
+
+    @Test
+    public void test(){
+        ArrayList arrayList = new ArrayList();
+        arrayList.add(123);
+        arrayList.add(456);
+        arrayList.add(789);
+        arrayList.add(987);
+        arrayList.add(-555);
+
+        System.out.println(arrayList);
+
+        //Collections.reverse(arrayList);
+        //Collections.shuffle(arrayList);
+        //Collections.sort(arrayList);
+        //Collections.swap(arrayList,1,2);
+
+        arrayList.add(123);
+        arrayList.add(123);
+        int frequency = Collections.frequency(arrayList, 123);
+        System.out.println("frequency = " + frequency);
+        System.out.println(arrayList);
+    }
+}
