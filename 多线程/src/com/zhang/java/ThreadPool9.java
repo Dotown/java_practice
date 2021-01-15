103343f3c3bc5499c2eb5540d1104d280fcfb393
Dotown <1242262161@qq.com>
2021-01-15 11:21:29 +0800

    1.15

---
+++ 多线程/src/com/zhang/java/ThreadPool9.java
@@ -0,0 +1,20 @@
+package com.zhang.java;
+
+/**
+ * 创建线程的方式四：使用线程池
+ *
+ * 好处：
+ *  1.提高响应速度（减少了创建新线程的时间）
+ *  2.降低资源消耗（重复利用线程池中线程，不需要每次都创建）
+ *  3.便于线程管理
+ *    corePoolSize：核心池的大小
+ *    maximumPoolSize：最大线程数
+ *    keepAliveTime：线程没有任务时最多保持多长时间后会终止
+ *
+ * author PC
+ * create 2021-01-15-11:04
+ */
+public class ThreadPool9 {
+
+
+}
